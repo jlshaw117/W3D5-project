c4521508470478fd36672b8ff13dcbe42fbab0fc
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-09 11:51:02 -0800

    Add my_attr_accessor

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |variable|
+      define_method(variable) do
+        instance_variable_get("@#{variable}")
+      end
+
+      define_method("#{variable}=") do |value|
+        instance_variable_set("@#{variable}", value)
+      end
+
+    end
   end
 end
