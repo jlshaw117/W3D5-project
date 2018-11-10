522d5edd773938e90f965db3aaff1efbdea2cf7b
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-09 16:47:45 -0800

    Add where to searchable

--- active_record_lite/lib/02_searchable.rb
+++ active_record_lite/lib/02_searchable.rb
@@ -4,9 +4,26 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+
+    where_arr = params.map do |col, value|
+      "#{col} = '#{value}'"
+    end
+    where_str = where_arr.join(' AND ')
+
+
+
+
+    data = DBConnection.execute(<<-SQL)
+    SELECT *
+    FROM #{table_name}
+    WHERE #{where_str}
+    SQL
+
+    self.parse_all(data)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
